36f61ff19ac4ec7f8af0b6d79041b1e5441276d5
allanvinicius <allanviniciosdemoura@gmail.com>
2025-02-10 14:50:22 -0300

    feat: change main

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import "@radix-ui/themes/styles.css";
 import { GlobalStyle } from "./styles/global";
 import { Theme } from "@radix-ui/themes";
 
+import "@radix-ui/themes/styles.css";
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <>
-      <Theme appearance="dark">
-        <App />
-      </Theme>
-    </>
+  <>
+    <Theme appearance="dark">
+      <App />
+    </Theme>
 
     <GlobalStyle />
-  </StrictMode>
+  </>
 );
